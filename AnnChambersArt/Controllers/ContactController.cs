48d43a85f3a1ad1aa5519b3772dfb6b1168ffe25
martinchambers <martinclivechambers@gmail.com>
2015-05-23 14:06:42 +0100

    reimplementing controller/view changes to old build 2

---
+++ AnnChambersArt/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnnChambersArt.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.Mode = "_shopwindow";
+            return View();
+        }
+    }
+}
